0ad01eacf7e5600568901d55fa413157e620a1ef
Ricardo Cisterna <r.cisternasantos@gmail.com>
2018-09-25 23:52:55 -0300

    facial recognition live demo

--- rasalas/demo.py
+++ rasalas/demo.py
@@ -3,96 +3,108 @@
 #
 '''Demo de reconocimiento facial.'''
 
-import face_recognition
+import os
+import re
+
+import click
+import face_recognition as fr
+import numpy as np
 import cv2
 
-def main():
-    # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-    # other example, but it includes some basic performance tweaks to make things run a lot faster:
-    #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-    #   2. Only detect faces in every other frame of video.
 
-    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-    # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-    # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+def _scan_known_people(people_path):
+    """Escanea a las personas conocidas desde un directorio."""
+    known_names = []
+    known_face_encodings = []
 
-    # Get a reference to webcam #0 (the default one)
+    pattern = r'.*\.(jpg|jpeg|png)'
+    img_files = [os.path.join(people_path, f) for f in os.listdir(people_path) if re.match(pattern, f, flags=re.I)]
+
+    for file in img_files:
+        basename = os.path.splitext(os.path.basename(file))[0]
+        img = fr.load_image_file(file)
+        encodings = fr.face_encodings(img)
+
+        if len(encodings) > 1:
+            click.echo("WARNING: More than one face found in {}. Only considering the first face.".format(file))
+
+        if encodings:
+            known_names.append(basename)
+            known_face_encodings.append(encodings[0])
+        else:
+            click.echo("WARNING: No faces found in {}. Ignoring file.".format(file))
+
+    return known_names, known_face_encodings
+
+
+def live(scale=0.25, tolerance=0.6):
+    """Demo en vivo utilizando la cámara integrada."""
     video_capture = cv2.VideoCapture(0)
 
-    # Load a sample picture and learn how to recognize it.
-    imgs = []
-    imgs.append(face_recognition.load_image_file("/Users/rcisterna/Code/rasalas/img/cntx-lucas.png"))
-    imgs.append(face_recognition.load_image_file("/Users/rcisterna/Code/rasalas/img/cntx-ricardo.png"))
-    imgs.append(face_recognition.load_image_file("/Users/rcisterna/Code/rasalas/img/cntx-gonzalo-v.png"))
-    imgs.append(face_recognition.load_image_file("/Users/rcisterna/Code/rasalas/img/cntx-gonzalo-c.png"))
+    # Carga las imagenes de gente conocida desde un directorio
+    known_path = "/Users/rcisterna/Code/rasalas/img/"
+    known_face_names, known_face_encodings = _scan_known_people(known_path)
 
-    # Create arrays of known face encodings and their names
-    known_face_encodings = []
-    for img in imgs:
-        known_face_encodings.append(face_recognition.face_encodings(img)[0])
-    known_face_names = ["Lucas Torres", "Ricardo Cisterna", "Gonzalo Vergara", "Gonzalo Candia"]
-
-    # Initialize some variables
-    face_locations = []
-    face_encodings = []
-    face_names = []
     process_this_frame = True
-
     while True:
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
+        # Obtiene un nuevo frame
+        _, frame = video_capture.read()
 
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Escala el frame, usando el parámetro 'scale'
+        small_frame = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        # Convierte la imagen de BGR (usado por OpenCV) a RGB (usado por face_recognition)
         rgb_small_frame = small_frame[:, :, ::-1]
 
-        # Only process every other frame of video to save time
+        # Procesa frames por medio
         if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            # Encuentra todas las caras en el video y las codifica para ser comparadas
+            face_locations = fr.face_locations(rgb_small_frame)
+            face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
 
             face_names = []
             for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
+                # Obtiene las "distancias" entre el rostro del video y los rostros conocidos
+                distances = fr.face_distance(known_face_encodings, face_encoding)
+                # Si las distancias son superiores a cierta tolerancia, se marcan con np.nan
+                distances[distances > tolerance] = np.nan
 
-                # If a match was found in known_face_encodings, just use the first one.
-                if True in matches:
-                    first_match_index = matches.index(True)
-                    name = known_face_names[first_match_index]
+                if np.all(np.isnan(distances)):
+                    # Si no existe ninguna distancia, es un rostro desconocido
+                    name = "Unknown"
+                else:
+                    # Si existe al menos una distancia, obtiene la menor
+                    index = np.nanargmin(distances)
+                    name = known_face_names[index]
 
                 face_names.append(name)
 
         process_this_frame = not process_this_frame
 
-
-        # Display the results
+        # Muestra los resultados en el video
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+            # Escala las posiciones de vuelta al tamaño original
+            scale_inv = 1 / scale
+            top = int(top * scale_inv)
+            right = int(right * scale_inv)
+            bottom = int(bottom * scale_inv)
+            left = int(left * scale_inv)
 
-            # Draw a box around the face
+            # Dibuja una caja roja en la cara
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            # Draw a label with a name below the face
+            # Dibuja un label con el nombre bajo la cara
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-        # Display the resulting image
+        # Muestra la imagen resultante
         cv2.imshow('Video', frame)
 
-        # Hit 'q' on the keyboard to quit!
+        # Si 'q' es presionado, termina el ciclo
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release handle to the webcam
+    # Libera los recursos
     video_capture.release()
     cv2.destroyAllWindows()
